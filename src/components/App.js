66a0df6305fbe274b71edbbbc8ddeb141fe52874
Amit Hariyale <57282097+Amithariyale@users.noreply.github.com>
2024-02-10 00:12:24 +0530

    solution

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,17 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+  let [cnt, setCnt] = useState(0);
+  const incrementCnt = () => {
+    setCnt(cnt + 1);
+  };
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <p>Button clicked {cnt} times</p>
+      <button onClick={incrementCnt}>Click me</button>
     </div>
-  )
+  );
 }
 
 export default App
